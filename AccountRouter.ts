5dbe7d238b5b16669e639d5088bdb0f3ccfe6251
VladVerlov <145294750+VladVerlov@users.noreply.github.com>
2023-09-17 23:57:17 +0300

    Add files via upload

---
+++ AccountRouter.ts
@@ -0,0 +1,48 @@
+import { router, publicProcedure } from "../trpc";
+import AccountService from "../services/AccountService";
+import { UserSchema,addBalanceSchema,buyCoinSchema,sellCoinSchema } from "../schemas/AccountSchemas";
+import { z } from "zod";
+
+const accountService = new AccountService();
+
+export const AccountRouter = router({
+  createUser: publicProcedure.input(UserSchema).mutation(async (req) => {
+     const {input} = req
+     const res = await accountService.createUser(input)
+     return res
+  }),
+  checkIfUserExists:publicProcedure.input(UserSchema).query(async (req) =>{
+    const {input} = req
+    const res = await accountService.checkIfUserExists(input.username,input.password)
+    return res
+  }),
+  addBalanceToUser:publicProcedure.input(addBalanceSchema).mutation(async (req) =>{
+    const {input} = req
+    const res = await accountService.addBalanceToUser(input.money,input.userId)
+    return res
+  }),
+  buyCoin:publicProcedure.input(buyCoinSchema).mutation(async (req) =>{
+    const {input} = req
+    const res = await accountService.buyCoin(input.userId,input.coinName,input.coinPrice,input.count)
+    return res
+  }),
+  getUserCoins:publicProcedure.input(z.number()).query(async (req) =>{
+    const {input} = req
+    const res = await accountService.getUserCoins(input)
+    return res
+  }),
+  getUserId:publicProcedure.input(UserSchema).query(async(req)=>{
+    const {input} = req
+    const res = await accountService.getUserId(input.username,input.password)
+    return res
+  }),
+  getUserBalance:publicProcedure.input(z.number()).query(async (req)=>{
+    const {input} = req
+    const res = await accountService.getUserBalance(input)
+    return res
+  }),
+  sellCoin:publicProcedure.input(sellCoinSchema).mutation(async (req)=>{
+    const {input} = req
+    await accountService.sellCoin(input.coinName,input.userId,input.count,input.coinPrice)
+  }),
+});
